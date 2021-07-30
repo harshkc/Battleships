dfa69648e060aa0d47734d695ee00d23d0ba4892
harshkc <harsh.hkc1006@gmail.com>
2021-07-30 16:54:51 +0530

    UI: finished UI :art: :tada:

--- main.py
+++ main.py
@@ -1,20 +1,63 @@
 import os
 import pygame
 
+# creates a window with defined width & height
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space War")
+# set window title
+pygame.display.set_caption("BattleShips")
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# TODO: ship movement, space constraints, bullets firing, collision handling, health deprecation, winner text, restart logic
+
+# game elements constant
+FPS = 60
+SPACESHIP_HEIGHT, SPACESHIP_WIDTH = 55, 40
+
+# defining spaceships look and orientation
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join("Assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join("Assets", "spaceship_red.png"))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+SPACE_BG = pygame.transform.scale(pygame.image.load(
+    os.path.join("Assets", "space.png")), (WIDTH, HEIGHT))
+
+
+CENTRE_BORDER = pygame.Rect(WIDTH//2-5, 0, 10, HEIGHT)
+
+
+def draw_window(yellow, red):
+    WIN.blit(SPACE_BG, (0, 0))
+    pygame.draw.rect(WIN, BLACK, CENTRE_BORDER)
+
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    pygame.display.update()
 
 
 def main():
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw_window(yellow, red)
 
-        clock.tick(60)
     pygame.quit()
 
 
